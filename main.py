38b4c0c7712f3e4f2c9baca1321d2ee97ab21d67
Aleksa Ognjanovic <alexa.ognjanovic@gmail.com>
2020-04-07 13:21:34 +0200

    Started working on code

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
+#!/usr/bin/env python3
 import os
 import string
 import random
-from datetime import datetime
-from flask import Flask, url_for, redirect, render_template, request, abort
-from flask_dance.contrib.github import make_github_blueprint, github
+import datetime
+from flask import Flask, url_for, redirect, render_template, request, jsonify
+from flask_apscheduler import APScheduler
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_login import LoginManager, UserMixin, login_required, login_user,\
     logout_user, current_user
+# from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
 
-
 # Blueprints
-github_client_secret = os.environ.get("GITHUB_CLIENT_SECRET")
-github_client_id = os.environ.get("GITHUB_CLIENT_ID")
+# github_client_secret = os.environ.get("GITHUB_CLIENT_SECRET")
+# github_client_id = os.environ.get("GITHUB_CLIENT_ID")
 # google_client_secret = os.environ.get("GOOGLE_CLIENT_SECRET")
 # google_client_id = os.environ.get("GOOGLE_CLIENT_ID")
 
 # App Config
-app.config["SECRET_KEY"] = github_client_secret
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SECRET_KEY"] = os.environ.get("SQL_SECRET_KEY")
+app.config["SCHEDULER_API_ENABLED"] = True
+# app.config["SECRET_KEY"] = github_client_secret
 # app.conifg["GOOGLE_OAUTH_CLIENT_ID"] = google_client_id
 # app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = google_client_secret
 
 
 # Make github blueprint
-github_blueprint = make_github_blueprint(
-    client_id=github_client_id,
-    client_secret=github_client_secret
-)
-app.register_blueprint(github_blueprint, url_prefix='/login/github')
+# github_blueprint = make_github_blueprint(
+#     client_id=github_client_id,
+#     client_secret=github_client_secret
+# )
+# app.register_blueprint(github_blueprint, url_prefix='/login/github')
 
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
+
 bcrypt = Bcrypt(app)
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 
 def generate_token(leng=64):
     return ''.join([random.choice(string.ascii_lowercase+"0123456789")
-                    for i in range(64)])
+                    for i in range(leng)])
 
 
-# User database
+def get_expire_date():
+    return datetime.datetime.now() + datetime.timedelta(minutes=10)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
@@ -55,19 +65,41 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(60), nullable=False)
     token = db.Column(db.String(64), nullable=False,
                       unique=True, default=generate_token)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    date_created = db.Column(db.DateTime, default=datetime.now)
+
+class Code(db.Model):
+    code = db.Column(db.String(8),
+                     primary_key=True,
+                     unique=True,
+                     nullable=False)
+    device_code = db.Column(db.String(128), unique=True, nullable=False)
+    ip_address = db.Column(db.String(15), unique=True, nullable=False)
+    expire_date = db.Column(db.DateTime, default=get_expire_date)
 
 
-# load user by id
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Home route
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@scheduler.task('interval', id='do_job_1', seconds=1, misfire_grace_time=900)
+def delete_expired():
+    expired = Code.query.filter(
+        Code.expire_date <= datetime.datetime.now()
+    ).all()
+
+    if expired:
+        print(f"Deleting {len(expired)} expired code(s)")
+
+    for code in expired:
+        db.session.delete(code)
+        db.session.commit()
 
 
 @app.route("/logout")
@@ -76,12 +108,42 @@ def logout():
     logout_user(current_user)
 
 
+@app.route("/code", methods=["GET", "POST"])
+def code():
+    if request.method == "GET":
+        code = generate_token(8)
+        device_code = generate_token(128)
+        ip = request.remote_addr
+
+        old_code = Code.query.filter_by(ip_address=ip).first()
+        if old_code:
+            old_code.code = code
+            old_code.device_code = device_code
+        else:
+            new_code = Code(
+                code=code,
+                device_code=device_code,
+                ip_address=ip,
+                expire_date=get_expire_date()
+            )
+            db.session.add(new_code)
+
+        db.session.commit()
+
+        return jsonify({
+            "message": "success",
+            "response": "200",
+            "code": code,
+            "device_code": device_code
+        }), 200
+
+
 @app.route("/token", methods=["GET"])
 @login_required
 def token():
     return current_user.token
 
-# Login
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -94,15 +156,15 @@ def login():
 
             if bcrypt.check_password_hash(pass_hashed, password):
                 login_user(user)
-                return "<h1>Yay</h1>"
+                return jsonify({"message": "success", "response": "200"}), 200
             else:
-                abort(401)
+                return jsonify({"message": "password is wrong", "response": "401"}), 401
         else:
-            abort(401)
+            jsonify({"message": "username doesn't exist", "response": "401"}), 401
     else:
         return render_template("login.html")
 
-# Register
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -122,32 +184,13 @@ def register():
             db.session.add(new_user)  # Add to database
             db.session.commit()  # Commit changes
         except IntegrityError:
-            return "<h1>Already exists!</h1>"  # If Unique rule is broke
-        return "<h1>Yay</h1>"  # Success
+            # If Unique rule is broke
+            return jsonify({"message": "Already exists", "response": "401"}), 401
+        # Success
+        return jsonify({"message": "success", "response": "200"}), 200
     else:
         return render_template("register.html")
 
 
-# @app.route('/login/github')
-# def github_login():
-#     if not github.authorized:
-#         return redirect(url_for('github.login'))
-#         account_info = github.get('/user')
-#     else:
-#         if account_info.ok:
-#             account_info_json = account_info.json()
-#             try:
-#                 new_user = User(
-#                     username=account_info_json["login"],
-#                     email=account_info_json["email"],
-#                     name=account_info_json["name"],
-#                     github=True,
-#                 )
-#                 db.session.add(new_user)
-#                 db.session.commit()
-#             except IntegrityError:
-#                 return "User already exists!"
-#             return "User created!"
-#     return '<h1>Request failed!</h1>'
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=80)
